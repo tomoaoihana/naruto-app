1a3aab27396d89c8bc5ffc62b16e784c01542da0
tomoaoihana <tamu.kusanishi@gmail.com>
2024-07-16 06:18:46 +0900

    axsiosでAPIコール

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import { useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
+
+  //ここはじゃけいさん的コード
+  // axios.get("https://narutodb.xyz/api/character").then((res) => {
+  //   console.log(res.data);
+  // });
+
+  //ここはくるしばさんコード
+  const fetchCharacters = async () => {
+    const apiUrl = "https://narutodb.xyz/api/character";
+
+    const result = await axios.get(apiUrl);
+    console.log(result);
+  };
   return <div className="App">hello world</div>;
 }
 
