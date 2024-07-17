368d63c99d851bd13fac63e4f405413e65de8df7
tomoaoihana <tamu.kusanishi@gmail.com>
2024-07-18 08:55:11 +0900

    NARUTO完成！

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 
+const limit = 100;
+
 function App() {
   const [characters, setCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  //最初のページは１なので、初期値は１
+  const [page, setPage] = useState(1);
   useEffect(() => {
     fetchCharacters();
   }, []);
@@ -14,41 +19,87 @@ function App() {
   // });
 
   //ここはくるしばさんコード
-  const fetchCharacters = async () => {
+  const fetchCharacters = async (page) => {
+    setIsLoading(true);
     const apiUrl = "https://narutodb.xyz/api/character";
 
-    const result = await axios.get(apiUrl);
+    const result = await axios.get(apiUrl, { params: { page, limit } });
     setCharacters(result.data.characters);
     console.log(result);
+    setIsLoading(false);
+  };
+
+  //Nextボタンをクリックした時の処理
+  const handleNext = async () => {
+    const nextPage = page + 1;
+    await fetchCharacters(nextPage);
+    setPage(nextPage);
+  };
+
+  //Prevボタンをクリックした時の処理
+  const handlePrev = async () => {
+    const nextPage = page - 1;
+    await fetchCharacters(nextPage);
+    setPage(nextPage);
   };
   return (
     <div className="container">
-      <main>
-        <div className="cards-container">
-          {characters.map((character) => {
-            return (
-              <div className="card" key={character.id}>
-                <img
-                  src={
-                    character.images[0] != null
-                      ? character.images[0]
-                      : "dummy.png"
-                  }
-                  alt={character.name}
-                  className="card-image"
-                />
-                <div className="card-content">
-                  <h3 className="card-title">{character.name}</h3>
-                  <p className="card-description"></p>
-                  <div className="card-footer">
-                    <span></span>
+      <div className="header">
+        <div className="header-content">
+          <img src="logo.png" alt="" className="logo" />
+        </div>
+      </div>
+      {isLoading ? (
+        <div className="loading">Now Loading...</div>
+      ) : (
+        <main>
+          <div className="cards-container">
+            {characters.map((character) => {
+              return (
+                <div className="card" key={character.id}>
+                  <img
+                    src={
+                      character.images[0] != null
+                        ? character.images[0]
+                        : "dummy.png"
+                    }
+                    alt={character.name}
+                    className="card-image"
+                  />
+                  <div className="card-content">
+                    <h3 className="card-title">{character.name}</h3>
+                    <p className="card-description">
+                      {character.debut != null
+                        ? character.debut.appearsIn
+                        : "No Description"}
+                    </p>
+                    <div className="card-footer">
+                      <span className="affiliation">
+                        {character.personal != null
+                          ? character.personal.affiliation
+                          : "No Affiliation"}
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
-      </main>
+              );
+            })}
+          </div>
+          <div className="pager">
+            <button className="prev" disabled={page === 1} onClick={handlePrev}>
+              Prev
+            </button>
+            <span className="page-number">{page}</span>
+            <button
+              className="next"
+              disabled={limit > characters.length}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </main>
+      )}
     </div>
   );
 }
